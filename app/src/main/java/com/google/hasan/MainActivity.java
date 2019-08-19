bdd7121213d6c239349da7de1751ccc42cd0782e
unknown <almas.ahmad2442@gmial.com>
2019-08-20 00:27:11 +0700

    list view

--- app/src/main/java/com/google/hasan/MainActivity.java
+++ app/src/main/java/com/google/hasan/MainActivity.java
@@ -1,7 +1,11 @@
 package com.google.hasan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+
+        setSupportActionBar(toolbar);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Membaca file menu dan menambahkan isinya ke action bar jika ada.
+        getMenuInflater().inflate(R.menu.menu_icon, menu);
+        return true;
+    }
+
+    public void pindahAbout (MenuItem about){
+        startActivity(new Intent(MainActivity.this,AboutActivity.class));
+    }
+
 }
