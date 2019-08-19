c455238ca33b424fd27526fbbcfba8205bd47f6b
unknown <almas.ahmad2442@gmial.com>
2019-08-20 01:31:24 +0700

    list view done

--- app/src/main/java/com/google/hasan/MainActivity.java
+++ app/src/main/java/com/google/hasan/MainActivity.java
@@ -6,8 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    int[] isi_gambar = {R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user,R.drawable.ic_user};
+
+    String [] isi_nama = {"Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas"};
+
+    String [] isi_Diskripsi = {"Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas","Almas"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_main);
 
         setSupportActionBar(toolbar);
+
+        listView = (ListView)findViewById(R.id.list_view);
+
+        list_adaptor adapter = new list_adaptor(MainActivity.this, isi_gambar, isi_nama, isi_Diskripsi);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                Bundle b = new Bundle();
+                intent.putExtra("key", i); //Your id
+                intent.putExtra("data",isi_nama[i]);
+                startActivity(intent);
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
